109cb17b80f43f0df506aeeef738bc3be42328bb
Miriamvdb <miriamvdb@live.nl>
2022-09-08 11:21:56 +0200

    Front/F4: FIXED User can login and see the dashboard

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getUserWithStoredToken } from "./store/user/thunks";
-import { Routes, Route, useNavigate } from "react-router-dom";
+import { Routes, Route, useNavigate, useLocation } from "react-router-dom";
 import { Navbar, MessageBox } from "./components";
 import { Dashboard, Login, SignUp } from "./pages";
 import { selectToken } from "./store/user/selectors";
@@ -10,12 +10,13 @@ import { selectToken } from "./store/user/selectors";
 export const App = () => {
   const dispatch = useDispatch();
 
-  const navigate = useNavigate();
   const token = useSelector(selectToken);
+  const location = useLocation();
+  const navigate = useNavigate();
 
   useEffect(() => {
     dispatch(getUserWithStoredToken());
-    if (token === null) {
+    if (token === null && location.pathname !== "/signup") {
       navigate("/login");
     }
   }, [dispatch, token, navigate]);
@@ -25,7 +26,7 @@ export const App = () => {
       <Navbar />
       <MessageBox />
       <Routes>
-        <Route path="/" element={<Dashboard />} />
+        <Route path="/" element={token ? <Dashboard /> : <></>} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/login" element={<Login />} />
       </Routes>
