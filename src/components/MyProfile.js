4d6d03468bd91e5f76fac429650045088eded9e1
Miriamvdb <miriamvdb@live.nl>
2022-09-08 23:35:30 +0200

    Front/F6: Display my user profile

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,9 +1,46 @@
-import { SubContainer, Title } from "../styled";
+import { useSelector } from "react-redux";
+import { selectUser } from "../store/user/selectors";
+import { ImgM, SubContainer, Text } from "../styled";
 
 export const MyProfile = () => {
+  const me = useSelector(selectUser);
+
+  if (!me) return;
+  const { image, firstName, lastName, line, position, backNumber } = me;
+
   return (
     <SubContainer style={{ flex: 1 }}>
-      <Title>My profile</Title>
+      {me ? (
+        <div>
+          <ImgM src={image} alt="" />
+          <Text
+            style={{
+              fontSize: "10rem",
+              position: "absolute",
+              opacity: "15%",
+              fontWeight: "bold",
+              top: "10.5rem",
+              left: "15rem",
+            }}
+          >
+            {backNumber === null ? "" : `${backNumber}`}
+          </Text>
+          <Text>
+            <b>
+              {firstName} {lastName}
+            </b>
+          </Text>
+          <Text>
+            <b>Line </b>
+            {line}
+          </Text>
+          <Text>
+            <b>Position </b> {position}
+          </Text>
+        </div>
+      ) : (
+        <Text>Loading...</Text>
+      )}
     </SubContainer>
   );
 };
