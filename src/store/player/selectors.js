b52dc3398f46ed276102cbe701076b87f9020095
Miriamvdb <miriamvdb@live.nl>
2022-09-09 11:52:50 +0200

    Front/F1: Players sorted (line)

--- src/store/player/selectors.js
+++ src/store/player/selectors.js
@@ -1,1 +1,9 @@
-export const selectAllPlayers = (reduxState) => reduxState.player.allPlayers;
+// export const selectAllPlayers = (reduxState) => reduxState.player.allPlayers;
+
+// Select all players - sorted by firstName
+export const selectAllPlayers = (reduxState) => {
+  const allPlayers = [...reduxState.player.allPlayers];
+  return allPlayers.sort((playerA, playerB) =>
+    playerA.line.localeCompare(playerB.line)
+  );
+};
