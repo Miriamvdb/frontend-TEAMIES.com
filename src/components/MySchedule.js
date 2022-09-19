19d79dcae9f51b80f04676d3465dd9aa25af82de
Miriamvdb <miriamvdb@live.nl>
2022-09-19 10:24:09 +0200

    Front/F10: FIXED BUG My schedule - events are sorted by date

--- src/components/MySchedule.js
+++ src/components/MySchedule.js
@@ -1,11 +1,11 @@
 import { useSelector } from "react-redux";
-import { selectMySchedule } from "../store/user/selectors";
+import { selectUser } from "../store/user/selectors";
 import { SubContainer, Text, TitleH2 } from "../styled";
 import { Roller } from "react-awesome-spinners";
 import { MyScheduleCard } from "./MyScheduleCard";
 
 export const MySchedule = () => {
-  const mySchedule = useSelector(selectMySchedule);
+  const me = useSelector(selectUser);
 
   return (
     <SubContainer style={{ flex: 1 }}>
@@ -16,29 +16,34 @@ export const MySchedule = () => {
           height: "24vh",
         }}
       >
-        {mySchedule ? (
-          mySchedule.length === 0 ? (
+        {me && me.mySchedule ? (
+          me.mySchedule.length === 0 ? (
             <>
               <Text>Come on, get off that lazy ass!</Text>
               <Text>The trainer will be very disapointed...</Text>
               <Text>You like it to be a substitute?</Text>
             </>
           ) : (
-            mySchedule.map((mS, index) => {
-              return (
-                <div key={index}>
-                  <MyScheduleCard
-                    id={mS.id}
-                    date={mS.date}
-                    title={mS.title}
-                    home={mS.home}
-                    opponent={mS.opponent}
-                    startTime={mS.startTime}
-                    endTime={mS.endTime}
-                  />
-                </div>
-              );
-            })
+            [...me.mySchedule]
+              ?.sort(
+                (eventA, eventB) =>
+                  new Date(eventA.date) - new Date(eventB.date)
+              )
+              .map((mS, index) => {
+                return (
+                  <div key={index}>
+                    <MyScheduleCard
+                      id={mS.id}
+                      date={mS.date}
+                      title={mS.title}
+                      home={mS.home}
+                      opponent={mS.opponent}
+                      startTime={mS.startTime}
+                      endTime={mS.endTime}
+                    />
+                  </div>
+                );
+              })
           )
         ) : (
           <Roller />
--- src/store/user/selectors.js
+++ src/store/user/selectors.js
@@ -1,10 +1,2 @@
 export const selectToken = (state) => state.user.token;
 export const selectUser = (state) => state.user.profile;
-
-// Select my schedule - sorted by date
-export const selectMySchedule = (reduxState) => {
-  const myScheduleByDate = [...reduxState.user.profile.mySchedule];
-  return myScheduleByDate.sort(
-    (eventA, eventB) => new Date(eventA.date) - new Date(eventB.date)
-  );
-};
